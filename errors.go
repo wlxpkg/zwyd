8b8c8b03ff4b7814eec34a92949d5035dd995e66
Yabo <386139859@qq.com>
2020-01-14 09:58:08 +0800

    :bug:

--- errors.go
+++ errors.go
@@ -31,7 +31,7 @@ var Errs = map[string]Errors{
 	"ERR_IDGEN_FAIL":         Errors{2404, "id生成失败"},
 
 	"ERR_ALBUM_GROUP_NAME_EMPTY":        Errors{15000, "请输入团队名称"},
-	"ERR_ALBUM_GROUP_NOT_EXIST":         Errors{15001, "找不到团队，请核实团队暗号"},
+	"ERR_ALBUM_GROUP_NOT_EXIST":         Errors{15001, "未找到团队，请核实团队暗号"},
 	"ERR_ALBUM_GROUP_HAS_JOIN":          Errors{15002, "您已加入该团队"},
 	"ERR_ALBUM_GROUP_CREATE_OVER_LIMIT": Errors{15003, "您创建的团队已达上限"},
 	"ERR_ALBUM_GROUP_JOIN_OVER_LIMIT":   Errors{15004, "您加入的团队已达上限"},
